0cb5b3b345c9a1acbe771252f23c72367cd4433d
Artsem Paulenka <itemka2503@gmail.com>
2024-10-11 13:36:23 +0200

    Fix test for App component

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,10 +1,16 @@
 import { render } from "@testing-library/react";
 import App from './App.tsx';
 
+const mockedScoreBoard = 'ScoreBoard';
+
+jest.mock('../ScoreBoard/ScoreBoard.tsx', () => ({
+  ScoreBoard: () => <div>{mockedScoreBoard}</div>,
+}));
+
 describe('App', () => {
-  it('should render', () => {
+  it('should render component', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Vite + React + TS')).toBeInTheDocument();
+    expect(getByText(mockedScoreBoard)).toBeVisible();
   });
 });
