aac94e9ff7bff1b2d7e2389950bf2c9814e9049f
Artsem Paulenka <itemka2503@gmail.com>
2024-10-12 16:51:15 +0200

    Update README.md file

--- src/helpers/sortMatchesByTotalScore/sortMatchesByTotalScore.ts
+++ src/helpers/sortMatchesByTotalScore/sortMatchesByTotalScore.ts
@@ -1,14 +1,14 @@
 import { Match } from "../../types/scoreBoard.ts";
 
 export function sortMatchesByTotalScore(matches: Match[]): Match[] {
-    return matches.slice().sort((a, b) => {
-      const totalScoreA = a.homeScore + a.awayScore;
-      const totalScoreB = b.homeScore + b.awayScore;
+  return matches.slice().sort((a, b) => {
+    const totalScoreA = a.homeScore + a.awayScore;
+    const totalScoreB = b.homeScore + b.awayScore;
 
-      if (totalScoreA === totalScoreB) {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-      }
+    if (totalScoreA === totalScoreB) {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    }
 
-      return totalScoreB - totalScoreA;
-    });
+    return totalScoreB - totalScoreA;
+  });
 }
